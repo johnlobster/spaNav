f4dd54631ecd020d53705ac1061e3cdc67b23861
John Webster <JohnLobster@comcast.net>
2019-10-24 13:28:45 -0700

    fixed _redirects, more information logging

--- example2/index.js
+++ example2/index.js
@@ -4,6 +4,9 @@ window.addEventListener("load", function () {
   console.log("Hello world");
 
   console.log("At address " + location.href);
+  console.log("path is " + location.path);
+  console.log("# is" + location.hash);
+  console.log("host is " + location.host);
 
   // footText is element where we can display url information on web page
   let footText = document.getElementById("spaFooterText");
